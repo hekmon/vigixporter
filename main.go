45bf8f2320d91080fc18aa22b262706ef2ccc6ea
Hekmon <edouardhur@gmail.com>
2021-02-01 17:10:31 +0100

    remove dead code

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/hekmon/vigixporter/hubeau"
@@ -46,24 +45,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func printResults(data []hubeau.Observation) {
-	// fmt.Printf("%#v\n", answer)
-	for _, obs := range data {
-		fmt.Printf("%s\t%s", obs.ObsDate, obs.StationCode)
-		switch obs.Type {
-		case hubeau.ObservationTypeLevel:
-			fmt.Printf("\t%.2fm", obs.ObsResultat/1000)
-		case hubeau.ObservationTypeFlow:
-			fmt.Printf("\t%.2fm3/s", obs.ObsResultat/1000)
-		default:
-			fmt.Printf("\t%f(unknown)", obs.ObsResultat)
-		}
-		if obs.SerieStatus != 4 {
-			fmt.Printf("\t(unknown status: %d)", obs.SerieStatus)
-		}
-		fmt.Println()
-	}
-	fmt.Printf("Printed %d results\n", len(data))
 }
