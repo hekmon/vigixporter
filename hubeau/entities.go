191508cad022bd67fec88c44f0834499c90ec062
Hekmon <edouardhur@gmail.com>
2021-02-01 13:00:11 +0100

    add predefined stations

---
+++ hubeau/entities.go
@@ -0,0 +1,10 @@
+package hubeau
+
+const (
+	// StationParis is the entity code for the station in Paris. https://www.vigicrues.gouv.fr/niv3-station.php?CdEntVigiCru=7&CdStationHydro=F700000103&GrdSerie=H&ZoomInitial=30
+	StationParis = "F700000103"
+	// StationAlfortville is the entity code for the station in Alfortville. https://www.vigicrues.gouv.fr/niv3-station.php?CdEntVigiCru=7&CdStationHydro=F490000104&GrdSerie=H&ZoomInitial=30
+	StationAlfortville = "F490000104"
+	// StationCreteil is the entity code for the station in Créteil. https://www.vigicrues.gouv.fr/niv3-station.php?CdEntVigiCru=7&CdStationHydro=F664000404&GrdSerie=H&ZoomInitial=30
+	StationCreteil = "F664000404"
+)
