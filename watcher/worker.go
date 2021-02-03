b4cf7a4b2e0187f1343c8e7a7b605cb610b79cf4
Hekmon <edouardhur@gmail.com>
2021-02-03 10:46:45 +0100

    log rewrite

--- watcher/worker.go
+++ watcher/worker.go
@@ -29,10 +29,10 @@ func (c *Controller) worker() {
 
 func (c *Controller) batch() {
 	// Get metrics
-	c.logger.Infof("[Watcher] current batch: getting new metrics...")
+	c.logger.Infof("[Watcher] batch: getting new metrics...")
 	oldestLastSeen := c.getOldestSeen()
 	if !oldestLastSeen.IsZero() {
-		c.logger.Debugf("[Watcher] current batch: requesting data from the oldest last seen we got: %v", oldestLastSeen)
+		c.logger.Debugf("[Watcher] batch: requesting data from the oldest last seen we got: %v", oldestLastSeen)
 	}
 	metrics, err := c.source.GetAllObservations(c.ctx, hydrometrie.ObservationsRequest{
 		EntityCode: c.stations,
@@ -41,65 +41,65 @@ func (c *Controller) batch() {
 		Sort:       hydrometrie.SortAscending,
 	})
 	if err != nil {
-		c.logger.Errorf("[Watcher] current batch: can't get metrics from hubeau: %s", err)
+		c.logger.Errorf("[Watcher] batch: can't get metrics from hubeau: %s", err)
 		return
 	}
 	if len(metrics) == 0 {
-		c.logger.Warning("[Watcher] current batch: we did not retreive any metrics")
+		c.logger.Warning("[Watcher] batch: we did not retreive any metrics")
 		return
 	}
-	c.logger.Infof("[Watcher] current batch: recovered %d values for %d stations", len(metrics), len(c.stations))
+	c.logger.Infof("[Watcher] batch: recovered %d values for %d stations", len(metrics), len(c.stations))
 	// Ingerate metrics
 	for index, metric := range metrics {
 		switch metric.Type {
 		case hydrometrie.ObservationTypeLevel:
 			if c.isLevelValueKnown(metric.StationCode, metric.ObsDate) {
-				c.logger.Debugf("[Watcher] current batch: index %d: level metric has a known date: skipping", index)
+				c.logger.Debugf("[Watcher] batch: index %d: level metric has a known date: skipping", index)
 				continue
 			}
-			c.logger.Debugf("[Watcher] current batch: index %d: adding a level metric (station: %s, time: %s, value: %f)",
+			c.logger.Debugf("[Watcher] batch: index %d: adding a level metric (station: %s, time: %s, value: %f)",
 				index, metric.StationCode, metric.ObsDate, metric.ObsResultat)
 			c.target.AddLevelValue(metric.SiteCode, metric.StationCode, metric.Latitude,
 				metric.Longitude, metric.ObsDate, metric.ObsResultat)
 			c.lastSeenLevelCandidate(metric.StationCode, metric.ObsDate)
 		case hydrometrie.ObservationTypeFlow:
 			if c.isFlowValueKnown(metric.StationCode, metric.ObsDate) {
-				c.logger.Debugf("[Watcher] current batch: index %d: flow metric has a known date: skipping", index)
+				c.logger.Debugf("[Watcher] batch: index %d: flow metric has a known date: skipping", index)
 				continue
 			}
-			c.logger.Debugf("[Watcher] current batch: index %d: adding a flow metric (station: %s, time: %s, value: %f)",
+			c.logger.Debugf("[Watcher] batch: index %d: adding a flow metric (station: %s, time: %s, value: %f)",
 				index, metric.StationCode, metric.ObsDate, metric.ObsResultat)
 			c.target.AddFlowValue(metric.SiteCode, metric.StationCode, metric.Latitude,
 				metric.Longitude, metric.ObsDate, metric.ObsResultat)
 			c.lastSeenFlowCandidate(metric.StationCode, metric.ObsDate)
 		default:
-			c.logger.Warningf("[Watcher] current batch: index %d: unknown metric type '%s' has been skipped: %+v",
+			c.logger.Warningf("[Watcher] batch: index %d: unknown metric type '%s' has been skipped: %+v",
 				index, metric.Type, metric)
 		}
 	}
-	c.logger.Debugf("[Watcher] current batch: updated lastseen:\n\tlevels: %+v\n\tflows: %+v", c.lastSeenLevels, c.lastSeenFlows)
+	c.logger.Debugf("[Watcher] batch: updated lastseen:\n\tlevels: %+v\n\tflows: %+v", c.lastSeenLevels, c.lastSeenFlows)
 	// Send them to victoria metrics
 	nbMetrics, nbValues, err := c.target.Send()
 	if err != nil {
-		c.logger.Errorf("[Watcher] current batch: can't send metrics to victoria metrics: %s", err)
+		c.logger.Errorf("[Watcher] batch: can't send metrics to victoria metrics: %s", err)
 		return
 	}
 	if nbMetrics == 0 {
-		c.logger.Info("[Watcher] current batch: no metric has been sent")
+		c.logger.Info("[Watcher] batch: no metric has been sent")
 	} else {
-		c.logger.Infof("[Watcher] current batch: successfully sent %d metrics containing %d values", nbMetrics, nbValues)
+		c.logger.Infof("[Watcher] batch: successfully sent %d metrics containing %d values", nbMetrics, nbValues)
 	}
 	// dump state to disk (just in case/to allow backup)
-	c.logger.Debug("[Watcher] current batch: dumping state to disk...")
+	c.logger.Debug("[Watcher] batch: dumping state to disk...")
 	if err := saveState(state{
 		LevelsBuffer:   c.target.GetLevelsBuffer(),
 		FlowsBuffer:    c.target.GetFlowsBuffer(),
 		LastSeenLevels: c.lastSeenLevels,
 		LastSeenFlows:  c.lastSeenFlows,
 	}); err != nil {
-		c.logger.Errorf("[Watcher] current batch: error while saving state to disk: %v", err)
+		c.logger.Errorf("[Watcher] batch: error while saving state to disk: %v", err)
 	} else {
-		c.logger.Info("[Watcher] current batch: state saved")
+		c.logger.Info("[Watcher] batch: state saved")
 	}
 }
 
