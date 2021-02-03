adaacd407656fcf9e2f4dbfc862a79a89afd89f7
Hekmon <edouardhur@gmail.com>
2021-02-03 11:04:21 +0100

    add signal handling

--- main.go
+++ main.go
@@ -3,22 +3,29 @@ package main
 import (
 	"context"
 	"os"
-	"time"
+	"os/signal"
+	"syscall"
 
 	"github.com/hekmon/vigixporter/hubeau"
 	"github.com/hekmon/vigixporter/watcher"
 	systemd "github.com/iguanesolutions/go-systemd/v5"
+	sysdnotify "github.com/iguanesolutions/go-systemd/v5/notify"
 
 	"github.com/hekmon/hllogger"
 )
 
 var (
-	logger *hllogger.HlLogger
+	logger        *hllogger.HlLogger
+	core          *watcher.Controller
+	mainLock      chan struct{}
+	mainCtx       context.Context
+	mainCtxCancel func()
 )
 
 func main() {
 	listOfStations := []string{hubeau.StationParis, hubeau.StationAlfortville, hubeau.StationCreteil}
 
+	// Setup the logger
 	_, systemdStarted := systemd.GetInvocationID()
 	var logFlags int
 	if !systemdStarted {
@@ -30,18 +37,60 @@ func main() {
 		SystemdJournaldCompat: systemdStarted,
 	})
 
-	ctx, ctxCancel := context.WithCancel(context.Background())
+	// Prepare main context for broadcasting the stop signal
+	mainCtx, mainCtxCancel = context.WithCancel(context.Background())
 
-	_, err := watcher.New(ctx, watcher.Config{
+	// Start core
+	var err error
+	if core, err = watcher.New(mainCtx, watcher.Config{
 		Stations: listOfStations,
 		Logger:   logger,
-	})
+	}); err != nil {
+		logger.Fatalf(1, "[Main] failed to instanciate the watcher: %s", err)
+	}
+	logger.Info("[Main] watcher started")
 
-	if err != nil {
-		logger.Fatalf(1, "Failed to instanciate the watcher: %s", err)
+	// Everything is ready, listen to signals to know when to stop
+	mainLock = make(chan struct{})
+	go handleSignals()
+
+	// Signal systemd we are ready if needed
+	if err = sysdnotify.Ready(); err != nil {
+		logger.Errorf("[Main] failed to notify systemd with ready signal: %s", err)
 	}
 
-	time.Sleep(10*time.Minute + 30*time.Second)
-	ctxCancel()
-	time.Sleep(30 * time.Second)
+	// Let's go to sleep while others do their work
+	<-mainLock
+}
+
+func handleSignals() {
+	var (
+		sig os.Signal
+		err error
+	)
+	// If we exit, allow main goroutine to do so
+	defer close(mainLock)
+	// Register signals
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, syscall.SIGTERM, syscall.SIGINT)
+	// Waiting for signals to catch
+	for {
+		sig = <-signalChannel
+		switch sig {
+		case syscall.SIGTERM:
+			fallthrough
+		case syscall.SIGINT:
+			logger.Infof("[Main] signal '%v' caught: cleaning up before exiting", sig)
+			if err = sysdnotify.Stopping(); err != nil {
+				logger.Errorf("[Main] can't send systemd stopping notification: %v", err)
+			}
+			// Cancel main ctx & wait for core to end
+			mainCtxCancel()
+			core.WaitStopped()
+			logger.Debugf("[Main] signal '%v' caught: watcher stopped: unlocking main goroutine to exit", sig)
+			return
+		default:
+			logger.Warningf("[Main] signal '%v' caught but no process set to handle it: skipping", sig)
+		}
+	}
 }
